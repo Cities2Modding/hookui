4f3fe805fdf5150b6859b2403a53c6e4ef3d79c9
Captain-Of-Coit <admin@captains-haven.org>
2023-12-07 20:07:18 +0100

    Make $Panel managed by hookui rather than mod authors
    Only happens when using .body in registerPanel rather than .component

--- ui_lib/loader.jsx
+++ ui_lib/loader.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {$Panel} from 'hookui-framework'
 
 const loadExtensions = (extensions) => {
     extensions.forEach(extension => {
@@ -45,15 +46,24 @@ const $Loader = ({ react }) => {
         }
     }, [])
 
-    const components = Object.keys(window._$hookui.__registeredPanels)
+    const panels = Object.keys(window._$hookui.__registeredPanels)
     .filter(i => visibleComponents.includes(i))
-    .map(k => window._$hookui.__registeredPanels[k].component)
+    .map(k => window._$hookui.__registeredPanels[k])
 
-    const to_render = components.map(c => {
-        const el = React.createElement(c, {react: react})
-        return <div key={c.id}>
-            {el}
-        </div>
+    const to_render = panels.map(p => {
+        if (p.component) {
+            const el = React.createElement(p.component, {react: react})
+            return <div key={p.id}>
+                {el}
+            </div>
+        } else if (p.body) {
+            const el = React.createElement(p.body, {react: react})
+            return <$Panel key={p.id} title={p.name} react={react}>
+                {el}
+            </$Panel>
+        } else {
+            throw new Error('Unknown panel structure for registerPanel, missing .body or .component')
+        }
     })
 
     return <div key="hookui_loader">
